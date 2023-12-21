1e677f22f07fca6b66cec8a30ba24ffb4bdbeb12
Vinicius Marinho <viniciusalexsander1@gmail.com>
2023-12-21 16:36:59 -0300

    feat: adicionando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+
+export const Header = () => {
+  const { data: session } = useSession();
+  return (
+    <header>
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Projetos
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Criar projeto
+          </a>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          Signed in as {session?.user?.name}
+          <button
+            onClick={() => signOut()}
+            className="text-sm font-semibold leading-6 text-gray-900 ml-3"
+          >
+            Sign out <span aria-hidden="true">&rarr;</span>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
