52ade96a535858001775dc4d6644c045cea01b6c
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-01-21 21:19:51 -0300

    teste

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,5 +1,7 @@
-import prisma from "@/shared/lib/prisma";
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
   const users = await prisma.user.findMany();
