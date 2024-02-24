72fd92b696d88fcbac1e96d6f2411a29b2f9421e
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-02-24 19:10:03 -0300

    fix: build

--- src/app/api/roles/route.ts
+++ src/app/api/roles/route.ts
@@ -1,16 +1,8 @@
-// import prisma from "@/shared/lib/prisma";
-import { getServerSession } from "next-auth";
+import prisma from "@/shared/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
+  const roles = await prisma.role.findMany();
 
-  // if (!session || !session.user) {
-  //   return NextResponse.json({ error: "session not found" }, { status: 401 });
-  // }
-
-  // const roles = await prisma.role.findMany();
-
-  // return NextResponse.json(roles);
+  return NextResponse.json(roles);
 }
