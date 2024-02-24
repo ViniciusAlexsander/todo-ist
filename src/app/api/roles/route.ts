1666f5757f11596709cc015b2052c8190f6099e7
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-02-24 19:12:02 -0300

    fix: build

--- src/app/api/roles/route.ts
+++ src/app/api/roles/route.ts
@@ -1,8 +1,22 @@
-import prisma from "@/shared/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const roles = await prisma.role.findMany();
-
-  return NextResponse.json(roles);
+  return NextResponse.json([
+    {
+      id: "clrsiecq50000weg7bzf1ief8",
+      description: "Owner",
+    },
+    {
+      id: "clrsiecq50001weg7mj0orq66",
+      description: "Admin",
+    },
+    {
+      id: "clrsiecq50002weg76ha3ut1u",
+      description: "Member",
+    },
+    {
+      id: "clrsiecq50002weg76ha3ut1u",
+      description: "teste",
+    },
+  ]);
 }
