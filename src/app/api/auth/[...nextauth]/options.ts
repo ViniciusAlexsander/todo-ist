f142d851f63c2455134ff1c86fa7d13df2a3c1a8
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-02-24 19:18:58 -0300

    fix: build

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,8 @@
-import prisma from "@/shared/lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
