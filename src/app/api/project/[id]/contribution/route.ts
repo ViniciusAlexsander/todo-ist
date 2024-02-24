5471c3451e38bc54f2138fb6babb03822e24ba26
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-02-24 12:24:00 -0300

    fix: build

--- src/app/api/project/[id]/contribution/route.ts
+++ src/app/api/project/[id]/contribution/route.ts
@@ -1,6 +1,5 @@
 // import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 interface IParams {
   id: string;
