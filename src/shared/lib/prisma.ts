884fd496ba515f06541fb5a4fe040e954b7dda22
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-02-24 17:14:48 -0300

    fix: build

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,9 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-globalForPrisma.prisma = prisma;
-
-export default prisma;
+export default client;
