5a3ba920e26eac501c0e4df7b0027c43d9c862f4
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-02-25 00:03:31 -0300

    fix: build

--- src/app/api/roles/route.ts
+++ src/app/api/roles/route.ts
@@ -1,5 +1,6 @@
-import prisma from "@/lib/prisma";
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
   const roles = await prisma.role.findMany();
